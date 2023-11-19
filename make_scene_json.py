98fd70d9705034ed8210453c0ef4d8895eff3c2d
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-19 20:28:53 +0900

    Added tqdm

--- make_scene_json.py
+++ make_scene_json.py
@@ -1,7 +1,8 @@
 import json
 from pathlib import Path
-
-from utils import Config, assert_dir, assert_file
+from tqdm import tqdm
+from assert_utils import assert_dir, assert_file
+from python_utils import Config
 
 conf = Config("config.json")
 assert_file("config.json", ".json")
@@ -13,15 +14,19 @@ assert_dir(scene_dir)
 assert_file(label_file)
 
 data = {}
+bar = tqdm(total=sum(1 for d in scene_dir.iterdir() if d.is_dir()))
 
 for action in scene_dir.iterdir():
     files = [
-        str(file.name)
+        str(file.relative_to(scene_dir))
         for file in action.iterdir()
         if file.is_file() and file.suffix == conf.mix.scene.ext
     ]
 
     data[action.name] = files
+    bar.update(1)
+
+bar.close()
 
 with open(conf.mix.scene.list, "w") as f:
     json.dump(data, f)
