58c7be3113460b033e0996675c96f7a96df2fbca
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-28 15:23:39 +0900

    Updated make file list json

--- make_file_list_json.py
+++ make_file_list_json.py
@@ -1,22 +1,22 @@
 import json
 from pathlib import Path
+
+from assertpy.assertpy import assert_that
+from python_config import Config
 from tqdm import tqdm
-from assert_utils import assert_dir, assert_file
-from python_utils import Config
 
 conf = Config("config.json")
-assert_file("config.json", ".json")
-
 scene_dir = Path(conf.mix.scene.path)
 label_file = Path(conf.ucf101.class_index)
 
-assert_dir(scene_dir)
-assert_file(label_file)
+assert_that(scene_dir).is_directory().is_readable()
+assert_that(label_file).is_file().is_readable()
+n_actions = sum(1 for d in scene_dir.iterdir() if d.is_dir())
 
 data = {}
-bar = tqdm(total=sum(1 for d in scene_dir.iterdir() if d.is_dir()))
+bar = tqdm(total=n_actions)
 
-for action in scene_dir.iterdir():
+for action in sorted(scene_dir.iterdir()):
     files = [
         str(file.relative_to(scene_dir))
         for file in action.iterdir()
@@ -29,4 +29,4 @@ for action in scene_dir.iterdir():
 bar.close()
 
 with open(conf.mix.scene.list, "w") as f:
-    json.dump(data, f)
+    json.dump(data, f, indent=2)
