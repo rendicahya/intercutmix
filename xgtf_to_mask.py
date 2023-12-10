f0c0c3b839ebdd5df4921a44e70e849c8ff644d0
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-12-10 22:11:45 +0900

    Merging xgtf parser

--- xgtf_to_mask.py
+++ xgtf_to_mask.py
@@ -1,13 +1,83 @@
 from pathlib import Path
+from typing import Union
 
 import cv2
 import numpy as np
-import xgtf_parser
 from assertpy.assertpy import assert_that
+from bs4 import BeautifulSoup
 from python_config import Config
 from python_file import count_files
 from python_video import video_info
 from tqdm import tqdm
+
+
+def parse_xgtf(path: Union[Path, str], action_only: bool = False):
+    assert_that(path).is_file().is_readable()
+
+    with open(path) as f:
+        try:
+            soup = BeautifulSoup(f, "xml")
+        except:
+            print("Error reading", path)
+            return None
+
+    all_bbox = {}
+    data = soup.find("data")
+
+    if data is None:
+        return None
+
+    for sourcefile in data.find_all("sourcefile"):
+        for person in sourcefile.find_all("object", {"name": "PERSON"}):
+            person_id = int(person["id"])
+
+            if person_id in all_bbox:
+                continue
+
+            person_locations = person.find("attribute", {"name": "Location"})
+            person_bbox = {}
+            person_action = person.find("data:bvalue", {"value": "true"})
+
+            if not person_action:
+                continue
+
+            act_start, act_end = [int(i) for i in person_action["framespan"].split(":")]
+
+            for bbox in person_locations.find_all("data:bbox"):
+                start, end = [int(i) for i in bbox["framespan"].split(":")]
+
+                if action_only:
+                    if act_start <= start <= act_end or act_start <= end <= act_end:
+                        start = max(start, act_start)
+                        end = min(end, act_end)
+
+                        for frame in range(start - 1, end):
+                            bbox_data = {
+                                frame: (
+                                    int(bbox["x"]),
+                                    int(bbox["y"]),
+                                    int(bbox["width"]),
+                                    int(bbox["height"]),
+                                )
+                            }
+
+                            person_bbox.update(bbox_data)
+                else:
+                    for frame in range(start - 1, end):
+                        bbox_data = {
+                            frame: (
+                                int(bbox["x"]),
+                                int(bbox["y"]),
+                                int(bbox["width"]),
+                                int(bbox["height"]),
+                            )
+                        }
+
+                        person_bbox.update(bbox_data)
+
+            all_bbox.update({person_id: person_bbox})
+
+    return all_bbox
 
 
 def main():
@@ -30,9 +100,7 @@ def main():
             if xgtf.suffix != ".xgtf":
                 continue
 
-            people_bbox = xgtf_parser.parse(
-                xgtf, action_only=conf.xgtf.mask.action_only
-            )
+            people_bbox = parse_xgtf(xgtf, action_only=conf.xgtf.mask.action_only)
 
             if not people_bbox:
                 continue
