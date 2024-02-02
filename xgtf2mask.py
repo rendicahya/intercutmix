2ae648043480f37c1e1d00c9cb67cf0609e0c090
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-02-02 12:34:39 +0900

    Falling back to the old per-frame masks

--- xgtf2mask.py
+++ xgtf2mask.py
@@ -80,58 +80,50 @@ def parse_xgtf(path: Union[Path, str], action_only: bool = False):
     return all_bbox
 
 
-def main():
+if __name__ == "__main__":
     conf = Config("config.json")
-    xgtf_dir = Path(conf.xgtf.path)
+    xgtf_dir = Path(conf.ucf101.xgtf.path)
     ucf101_dir = Path(conf.ucf101.path)
     ucf101_ext = conf.ucf101.ext
-    mask_ext = conf.xgtf.mask.ext
-    output_root = Path(conf.xgtf.mask.path)
+    output_root = Path(conf.ucf101.xgtf.mask.path)
     bar = tqdm(total=count_files(xgtf_dir))
 
     assert_that(xgtf_dir).is_directory().is_readable()
     assert_that(ucf101_dir).is_directory().is_readable()
 
-    for action in xgtf_dir.iterdir():
-        for xgtf in action.iterdir():
-            bar.set_description(xgtf.name)
+    for xgtf in xgtf_dir.glob("**/*.xgtf"):
+        bar.set_description(xgtf.stem)
 
-            if xgtf.suffix != ".xgtf":
-                continue
+        action = xgtf.parent.name
+        people_bbox = parse_xgtf(xgtf, action_only=conf.ucf101.xgtf.mask.action_only)
 
-            people_bbox = parse_xgtf(xgtf, action_only=conf.xgtf.mask.action_only)
+        if not people_bbox:
+            continue
 
-            if not people_bbox:
-                continue
+        video_path = ucf101_dir / action / (xgtf.with_suffix(ucf101_ext).name)
+        out_dir = output_root / action / xgtf.stem
 
-            video_path = ucf101_dir / action.name / (xgtf.with_suffix(ucf101_ext).name)
-            output_dir = output_root / action.name / xgtf.stem
+        out_dir.mkdir(parents=True, exist_ok=True)
 
-            output_dir.mkdir(parents=True, exist_ok=True)
+        info = video_info(video_path)
+        n_frames = info["n_frames"]
+        width, height = info["width"], info["height"]
 
-            info = video_info(video_path)
-            n_frames = info["n_frames"]
-            width, height = info["width"], info["height"]
+        for f in range(n_frames):
+            mask = np.zeros((height, width), np.uint8)
+            out_file = out_dir / (f"%05d.png" % f)
 
-            for i in range(n_frames):
-                mask = np.zeros((height, width), dtype=np.uint8)
-                output_file = output_dir / (f"%05d{mask_ext}" % i)
+            for person_id, person_bbox in people_bbox.items():
+                if f not in person_bbox:
+                    continue
 
-                for person_id, person_bbox in people_bbox.items():
-                    if i not in person_bbox:
-                        continue
+                x1, y1, w, h = person_bbox[f]
+                x2 = x1 + w
+                y2 = y1 + h
+                mask[y1:y2, x1:x2] = 255
 
-                    x1, y1, w, h = person_bbox[i]
-                    x2 = x1 + w
-                    y2 = y1 + h
-                    mask[y1:y2, x1:x2] = 255
+            cv2.imwrite(str(out_file), mask)
 
-                cv2.imwrite(str(output_file), mask)
-
-            bar.update(1)
+        bar.update(1)
 
     bar.close()
-
-
-if __name__ == "__main__":
-    main()
