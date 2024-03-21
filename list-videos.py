90fb4eb76ba4a4700d8a831ba71ddc306103189d
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-03-21 16:04:59 +0900

    Listing videos based on config

--- list-videos.py
+++ list-videos.py
@@ -8,29 +8,38 @@ from tqdm import tqdm
 conf = Config("config.json")
 relevancy_model = conf.relevancy.active.method
 relevancy_threshold = conf.relevancy.active.threshold
-video_root = Path(conf[conf.active.dataset].path).parent / "REPP"
+video_root = Path(conf[conf.active.dataset].path).parent
+min_mask_ratio = conf.cutmix.min_mask_ratio
+mode = conf.active.mode
 ext = conf.cutmix.output.ext
 
-for mode in "actorcutmix", "intercutmix":
-    video_dir = video_root / mode / "mix" / relevancy_model / str(relevancy_threshold)
+video_dir = (
+    video_root
+    / "REPP"
+    / mode
+    / "mix"
+    / str(min_mask_ratio)
+    / relevancy_model
+    / str(relevancy_threshold)
+)
 
-    assert_that(video_dir).is_directory().is_readable()
+assert_that(video_dir).is_directory().is_readable()
 
-    n_actions = conf[conf.active.dataset].n_classes
-    data = {}
-    bar = tqdm(total=n_actions)
+n_actions = conf[conf.active.dataset].n_classes
+data = {}
+bar = tqdm(total=n_actions)
 
-    for action in sorted(video_dir.iterdir()):
-        files = [
-            str(file.relative_to(video_dir))
-            for file in action.iterdir()
-            if file.is_file() and file.suffix == ext
-        ]
+for action in sorted(video_dir.iterdir()):
+    files = [
+        str(file.relative_to(video_dir))
+        for file in action.iterdir()
+        if file.is_file() and file.suffix == ext
+    ]
 
-        data[action.name] = files
-        bar.update(1)
+    data[action.name] = files
+    bar.update(1)
 
-    bar.close()
+bar.close()
 
-    with open(video_root / f"{mode}.json", "w") as f:
-        json.dump(data, f, indent=2)
+with open(video_dir / "list.json", "w") as f:
+    json.dump(data, f, indent=2)
