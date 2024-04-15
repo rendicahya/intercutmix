df36bf0fbed27a1134b203e67ba0e09aaaab6fbf
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-04-15 18:31:17 +0900

    Refactored and printed more info

--- list-scene.py
+++ list-scene.py
@@ -1,23 +1,32 @@
 import json
 from pathlib import Path
 
+import click
 from assertpy.assertpy import assert_that
 from config import settings as conf
 from tqdm import tqdm
 
-scene_dir = Path(conf.cutmix.input[conf.active.dataset].scene.path)
-scene_ext = conf.cutmix.input[conf.active.dataset].scene.ext
-json_out_path = conf.cutmix.input[conf.active.dataset].scene.list
-n_actions = conf[conf.active.dataset].n_classes
+dataset = conf.active.dataset
+scene_in_dir = Path(conf.cutmix.input[dataset].scene.path)
+scene_ext = conf.cutmix.input[dataset].scene.ext
+json_out_path = conf.cutmix.input[dataset].scene.list
+n_actions = conf[dataset].n_classes
 
-assert_that(scene_dir).is_directory().is_readable()
+print("Dataset:", dataset)
+print("Input:", mask_in_dir)
+print("Output:", video_out_dir)
+
+if not click.confirm("\nDo you want to continue?", show_default=True):
+    exit("Aborted.")
+
+assert_that(scene_in_dir).is_directory().is_readable()
 
 data = {}
 bar = tqdm(total=n_actions)
 
-for action in sorted(scene_dir.iterdir()):
+for action in sorted(scene_in_dir.iterdir()):
     files = [
-        str(file.relative_to(scene_dir))
+        str(file.relative_to(scene_in_dir))
         for file in action.iterdir()
         if file.is_file() and file.suffix == scene_ext
     ]
