67fd0deaf7ae514e9c3b841f6be8d3181b5e8001
Randy Cahya Wihandika <rendicahya@gmail.com>
2025-02-27 21:19:50 +0900

    Reading argument instead of config

--- tools/data/mask_ratio.py
+++ tools/data/mask_ratio.py
@@ -21,31 +21,39 @@ def calc_ratio(path):
     return path.stem, round(ratio, 4)
 
 
-ROOT = Path.cwd()
-DATASET = conf.active.dataset
-DETECTOR = conf.active.detector
-DET_CONFIDENCE = conf.detect[DETECTOR].confidence
-N_FILES = conf.datasets[DATASET].n_videos
-MASK_DIR = ROOT / "data" / DATASET / DETECTOR / str(DET_CONFIDENCE) / "detect" / "mask"
-MAX_WORKERS = conf.active.max_workers
-OUT_PATH = MASK_DIR / "ratio.json"
-data = {}
+@click.command()
+@click.argument(
+    "mask_dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path),
+)
+def main(mask_dir):
+    DATASET = conf.active.dataset
+    N_FILES = conf[DATASET].n_videos
+    MAX_WORKERS = conf.active.threads
+    OUT_PATH = mask_dir / "ratio.json"
+    data = {}
 
-print("Input:", MASK_DIR)
-print("Output:", OUT_PATH)
-print("n videos:", N_FILES)
+    print("Input:", mask_dir)
+    print("Output:", OUT_PATH)
+    print("n videos:", N_FILES)
 
-if not click.confirm("\nDo you want to continue?", show_default=True):
-    exit("Aborted.")
+    if not click.confirm("\nDo you want to continue?", show_default=True):
+        exit("Aborted.")
 
-assert_that(MASK_DIR).is_directory().is_readable()
+    assert_that(mask_dir).is_directory().is_readable()
 
-with ThreadPoolExecutor(max_workers=MAX_WORKERS) as exec:
-    jobs = {exec.submit(calc_ratio, path): path for path in MASK_DIR.glob("**/*.npz")}
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as exec:
+        jobs = {
+            exec.submit(calc_ratio, path): path for path in mask_dir.glob("**/*.npz")
+        }
 
-    for future in tqdm(as_completed(jobs), total=N_FILES, dynamic_ncols=True):
-        path_stem, ratio = future.result()
-        data[path_stem] = ratio
+        for future in tqdm(as_completed(jobs), total=N_FILES, dynamic_ncols=True):
+            path_stem, ratio = future.result()
+            data[path_stem] = ratio
 
-with open(OUT_PATH, "w") as f:
-    json.dump(data, f)
+    with open(OUT_PATH, "w") as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    main()
