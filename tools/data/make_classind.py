cf59c8e66aad2ab62bd0652e3393f0139d0f0a97
Randy Cahya Wihandika <rendicahya@gmail.com>
2025-03-01 10:23:12 +0900

    Simplified

--- tools/data/make_classind.py
+++ tools/data/make_classind.py
@@ -7,22 +7,18 @@ from pathlib import Path
 from assertpy.assertpy import assert_that
 from config import settings as conf
 
+ROOT = Path.cwd()
+DATASET = conf.active.dataset
+DATASET_DIR = ROOT / conf[DATASET].path
+OUT_FILE = DATASET_DIR.parent / "annotations/classInd.txt"
 
-def main():
-    ROOT = Path.cwd()
-    DATASET = conf.active.dataset
-    DATASET_DIR = ROOT / conf[DATASET].path
+assert_that(DATASET_DIR).is_directory().exists()
 
-    assert_that(DATASET_DIR).is_directory().exists()
+subdirs = [subdir.name for subdir in DATASET_DIR.iterdir() if subdir.is_dir()]
 
-    subdirs = [subdir for subdir in DATASET_DIR.iterdir() if subdir.is_dir()]
+subdirs.sort()
+OUT_FILE.parent.mkdir(exist_ok=True, parents=True)
 
-    subdirs.sort(key=lambda x: x.name)
-
-    with open(DATASET_DIR.parent / "annotations/classInd.txt", "w") as f:
-        for i, subdir in enumerate(subdirs, start=1):
-            f.write(f"{i} {subdir.name}\n")
-
-
-if __name__ == "__main__":
-    main()
+with open(OUT_FILE, "w") as f:
+    for i, subdir in enumerate(subdirs, start=1):
+        f.write(f"{i} {subdir}\n")
